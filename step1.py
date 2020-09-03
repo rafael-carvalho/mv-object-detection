da3c2a9dba4fdc4895cdf540f22c7631f4f47a8e
Rafael Carvalho <rafael.lcarvalho@gmail.com>
2020-09-02 18:01:53 -0700

    using elephant as first image

--- step1.py
+++ step1.py
@@ -8,7 +8,7 @@ from detect import detect_objects
 
 if __name__ == '__main__':
     # Determine the path to the image
-    image_path = 'sample_images/baseball.jpg'
+    image_path = 'sample_images/elephant.jpg'
 
     # Trigger the detection algorithm
     detections, classes, output_path = detect_objects(image_path, show_window=True, conf_threshold=.3)
